a1cf9205cfeedfa8f6579697828ebacecb0b9299
V205 <regards-cookers0a@icloud.com>
2023-11-03 20:12:07 -0700

    Update Trafficlight.py

--- Trafficlight.py
+++ Trafficlight.py
@@ -18,4 +18,27 @@ yellowLED.low()
 greenLED = Pin('P6_5')                    # create pin object for pin P6_5. This is for the green LED.
 greenLED.init(Pin.OUT, Pin.PULL_DOWN)      # set pin as output and enable pull-down resistor.
 greenLED.low()                             # set value low.
+print("Start program")
+while True: # repeat forever.
+  
+    redLED.low() # Turn off red for green light
+    yellowLED.low()  # turn off yellow      
+    greenLED.high() # turn on green
+    print("Green Light!")
+    time.sleep(5) # wait 5 seconds
+  
+    redLED.low() # turn off red light for yellow light
+    yellowLED.high()   # turn yellow light on     
+    greenLED.low() #turn green light off.
+    print("Yellow Light")
+    time.sleep(2) # wait 2 seconds
 
+    redLED.high() # turn on Red light
+    yellowLED.low()   # turn off yellow light     
+    greenLED.low() # turn off green lignt
+    print("Red Light")
+    time.sleep(5) # wait 5 seconds
+  
+    
+    
+    
